a4014d0ee18f07dda2b103946edcdc76743cb78f
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-07-31 16:04:32 +0100

    ninth to eleventh test

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -33,3 +33,39 @@ end
 def add_string_as_number(string1, string2)
   return string1.to_i + string2.to_i
 end
+
+# def test_month_by_number__month_1()
+#   month = number_to_full_month_name( 1 )
+#   assert_equal( "January", month )
+# end
+
+def number_to_full_month_name(month_number)
+  case month_number
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  else
+    return "This number is not valid"
+  end
+end
